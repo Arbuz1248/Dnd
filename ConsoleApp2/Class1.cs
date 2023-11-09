66ed6f98a1189cf302f194aa7039b3d9eba64aa7
torenets <werut140@gmail.com>
2023-11-09 21:21:18 +0300

    first commit

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp2
+{
+    internal class Class1
+    {
+    public static void Main(string[] args)
+        {
+            var people = new List<person>();
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("C:\\Users\\Mellor\\source\\repos\\ConsoleApp2\\ConsoleApp2\\people.xml");
+            XmlElement? xRoot = xmlDoc.DocumentElement;
+            if (xRoot != null )
+            {
+                foreach (XmlElement xnode in xRoot)
+                {
+                    person Person = new person();
+                    XmlNode? attr = xnode.Attributes.GetNamedItem("race");
+                    Person.Race = attr?.Value;
+                    foreach (XmlNode childNode in xnode.ChildNodes)
+                    {
+                        if(childNode.Name == "specifications")
+                        {
+                            Person.Specifications = childNode.InnerText;
+                        }
+                        if (childNode.Name == "speed")
+                        {
+                            Person.Speed = int.Parse(childNode.InnerText);
+                        }
+                        if (childNode.Name == "size")
+                        {
+                            Person.Size = childNode.InnerText;
+                        }
+                        if (childNode.Name == "age")
+                        {
+                            Person.Age = childNode.InnerText;
+                        }
+                        
+                    }
+                    people.Add(Person);
+                
+                }
+                foreach (var Person in people)
+                {
+                    Console.WriteLine("Раса: " + Person.Race + ", скорость: " + Person.Speed + ", размер:" + Person.Size + ", возраст: " + Person.Age + ", характеристики:" + Person.Specifications + ".");
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class person
+    {
+        public string Race { get; set; }
+        public int Speed { get; set; }
+        public string Age { get; set; }
+        public string Size { get; set;}
+        public string Specifications { get; set; }
+
+
+    }
+}
